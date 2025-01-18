3930530ff415367dcb6999cd9b21588505c61760
geovnefqj <83609201+geovnefqj@users.noreply.github.com>
2025-01-18 16:54:48 -0300

    Add files via upload

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,35 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amig = [];
+
+
+
+function adicionarAmigo(){
+    let input =  document.getElementById("amigo");
+    let amigo = input.value.trim();
+
+
+    if (amigo === "" ) {
+        alert('Por favor, digite um nome');
+
+
+    }
+    amig.push(amigo);
+
+    
+    let lista = document.getElementById("listaAmigos");
+    let item = document.createElement("li");
+    item.textContent = amigo;
+    lista.appendChild(item);
+
+    input.value = "";
+    input.focus();                  
+}
+function sortearAmigo() {
+    if (amig.length === 0) {
+        alert("por favor, adicione nomes para fazer o sorteio.");
+        return;
+    }
+
+    let sorteado = amig[Math.floor(Math.random() * amig.length)];
+    document.getElementById("resultado").textContent = "O amigo sorteado foi: " + sorteado;
+}
